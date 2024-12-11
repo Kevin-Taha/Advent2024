e2767559e4e6d6cdf3dd5ade5ae0c7f18d06511e
kevin-taha <tahakevin@gmail.com>
2024-12-10 22:45:57 -0800

    day 9 pt 2

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -101,21 +101,74 @@ uint64_t calcCheckSum(vector<int>& nums)
 uint64_t calcCheckSum2(vector<int>& nums)
 {
     auto memory = InputToMemoryChunkList(nums);
-    int id = -1;
 
+    vector<MemoryChunk*> fullChunks;
+    for(auto& chunk : memory)
+    {
+        if (chunk.id != -1) 
+        {
+            fullChunks.push_back(&chunk);
+        }
+    }
 
+    // Iterate through all file chunks, last to first. 
+    for(auto fileIter = fullChunks.rbegin(); fileIter != fullChunks.rend(); fileIter++)
+    {
+        auto sizeNeeded = (*fileIter)->chunkSize;
 
-   // return checkSum;
+        // Look for first empty chunk in the memory list that fits the file (and comes before it)
+        for(auto chunkIter = memory.begin(); chunkIter != memory.end(); chunkIter++)
+        {
+            if(chunkIter->id == (*fileIter)->id) break; // We iterated after the the original file. 
+
+            if (chunkIter->id == -1 && chunkIter->chunkSize >= sizeNeeded)
+            {
+                if (chunkIter->chunkSize == sizeNeeded)
+                {
+                    // We can use the full chunk, just transfer the id over
+                    chunkIter->id = (*fileIter)->id;
+                    (*fileIter)->id = -1;
+                    break;
+                }
+                else
+                {
+                    // We need to split the chunk up. new chunk size = old chunk size - sizeNeeded. Old chunk size = sizeNeeded
+                    memory.insert(next(chunkIter), {-1, chunkIter->chunkSize - sizeNeeded});
+
+                    chunkIter->id = (*fileIter)->id;
+                    chunkIter->chunkSize = sizeNeeded;
+                    (*fileIter)->id = -1;
+                    break;
+                }
+            }
+        }
+    }
+
+    vector<int>checkSumString;
+    int idx = 0;
+    uint64_t checkSum = 0;
+    for(auto& m : memory)
+    {
+        for(int i = 0; i < m.chunkSize; i++)
+        {
+            if(m.id != -1)
+            {
+                checkSum += idx * m.id;
+            }
+            idx++;
+        }
+    }
+
+    return checkSum;
 }
 
 int main()
 {
     auto numArr = readInput("input.txt");
-    //auto checkSum = calcCheckSum(numArr);
-    //cout << "Pt 1 Checksum: " << checkSum << endl;
-
-    auto checkSum = calcCheckSum2(numArr);
-    cout << "Pt 2 Checksum: " << checkSum << endl;
+    auto checkSum = calcCheckSum(numArr);
+    cout << "Pt 1 Checksum: " << checkSum << endl;
+    auto checkSum2 = calcCheckSum2(numArr);    
+    cout << "Pt 2 Checksum: " << checkSum2 << endl;
 
     return 0;
 }
