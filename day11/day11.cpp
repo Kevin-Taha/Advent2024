48e6b265fba619b1a4c78215c3c39f7776e72759
Kevin Taha <kevintaha@microsoft.com>
2024-12-11 20:14:40 -0800

    mild progress

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -1,13 +1,30 @@
 #include "../includes.h"
 
+struct StoneCount
+{
+    uint64_t zeroValueStones = 0;
+    uint64_t evenDigitStones = 0;
+    
+
+}
 
 
 
 
+vector<int> readInput(string inputFile)
+{
+    auto file = openFileSafe(inputFile);
+    string line;
+    getline(file, line);
+    return readLineIntoVector(line);
+}
 
 
 int main()
 {
+    auto input = readInput("input.txt");
+
+    printVector(input);
 
 
     return 0;
